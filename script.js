7911a40e627180877d92298e18a2f11c1f2b0a95
nitin <nitinsah20@gmail.com>
2025-06-29 03:13:17 +0530

    Initial Commit - Rract App

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+
+
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+const cityCondition = document.getElementById('city-condition');
+const cityWind = document.getElementById('city-wind');
+const lastUpdated = document.getElementById('last_updated');
+const weatherIcon = document.getElementById('weather-icon');
+
+
+async function getdata(cityName){
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=255d019269504abbb2e180416252406&q=${cityName}&aqi=yes`);
+    return await promise.json();
+};
+
+button.addEventListener('click', async ()=>{
+    const value = input.value;
+    const result = await getdata(value);
+    cityName.innerHTML = "City Name : " + `${result.location.name}, ${result.location.region}, ${result.location.country}`;
+    cityTime.innerHTML = "Time : " + result.location.localtime;
+    cityTemp.innerHTML = "Temperature : " + result.current.temp_c + "°C";
+    cityCondition.innerHTML = "Conditions : " + result.current.condition.text;
+    weatherIcon.src = "https:" + result.current.condition.icon;
+    cityWind.innerHTML = "Wind : " + result.current.wind_kph + " "+ "km/h";
+    lastUpdated.innerHTML = "last updated : " + result.current.last_updated;
+    
+});
